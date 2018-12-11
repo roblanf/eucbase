d2a2a5dd79dcb95cd2d99a791fa8eed5baa46196
Fred <fredjaya1@gmail.com>
2018-12-11 16:18:39 +1100

    gridMean outputs stdDev
    Adding SD outputs for speciesMean and gridMean. gridMean now outputs a dataframe with mean and SD. Tests passed.

--- tests/testthat/test_ras.R
+++ tests/testthat/test_ras.R
@@ -1,16 +1,16 @@
-context("rasMean")
-
-ex.ras.out <- rasMean(ex.in.spp, 100)
-ex.ras.out2 <- rasMean(ex.in.spp, 12.9823744)
+context("ras")
 
 e.n1 <- setExt(ex.in)
 ex.ras.outb.n1 <- rasMean(ex.in.n1, km = 100, e.n1)
+ex.rasSD.outb.n1 <- rasMean(ex.in.n1, km = 100, e.n1)
 
 #ex.ras.outb <- rasMean(ex.in.spp, 1) good error, but crashes testing??
 
 test_that('raster is output', {
-  expect_is(ex.ras.out, 'RasterBrick')
-  expect_is(ex.ras.out2, 'RasterBrick')
+  expect_is(rasMean(ex.in.spp, 100), 'RasterBrick')
+  expect_is(rasMean(ex.in.spp, 12.9823744), 'RasterBrick')
+  expect_is(rasSD(ex.in.spp, 100), 'RasterBrick')
+  expect_is(rasSD(ex.in.spp, 12.9823744), 'RasterBrick')
 })
 
 #test_that('small grid sizes give a blank dataframe??', {
@@ -19,4 +19,5 @@ test_that('raster is output', {
 
 test_that('no output produced when Species n = 1', {
   expect_output(sum(ex.ras.outb.n1@data@values), NA)
+  expect_output(sum(ex.rasSD.outb.n1@data@values), NA)
 })
