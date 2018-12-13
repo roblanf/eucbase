8f7943717aad3fc14b5dccc03f52afd8a54ddf05
Fred <fredjaya1@gmail.com>
2018-12-13 12:54:19 +1100

    remake of speciesMean passes tests. Example output files removed

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -4,40 +4,51 @@
 #4:ncol(data)  <- 4:ncol(data)
 
 ### Primary functions (called directly in user-functions) ###
-rMGrid <- function(data, km, e) {
+rMSpp <- function(data, km, e) {
+  # Check for species with layers that are all NAs
   checkNA(data)
+  
+  # Convert to spatial dataframe
   spdf <- spdf(data)
+  
+  # Calculate raster means
+  rasMean <- ras(spdf, mean, km, e)
+  
+  # Summarise values
+  out2 <- summarise_all(as.data.frame(rasMean@data@values), mean, na.rm = T)
+  out2 <- out2[rowSums(is.na(out2)) < ncol(out2),]
+  return(out2)
+  
+}
 
-    # Calculate raster means and sds
-    rasMean <- ras(spdf, mean, km, e)
-    rasSD   <- ras(spdf, sd,   km, e)
-    
-    # Export cell ID and coordinates
-    outM  <- data.frame(1:nrow(rasMean@data@values ), xyFromCell(rasMean, 1:nrow(rasMean@data@values )),  rasMean@data@values)
-    outSD <- data.frame(1:nrow(rasSD@data@values), xyFromCell(rasSD,   1:nrow(rasSD@data@values)), rasSD@data@values)
-    
-    # Rename columns
-    colnames(outM )[1]   <- colnames(outSD)[1] <- "cellID"
-    colnames(outM )[4:ncol(data)] <- paste("mean", colnames(data)[4:ncol(data)], sep = "_")
-    colnames(outSD)[4:ncol(data)] <- paste("SD"  , colnames(data)[4:ncol(data)], sep = "_")
-    
-    # Join mean and SD
-    out2 <- left_join(outM, outSD[, c(1, 4:ncol(data))], "cellID")
-    
-    # Remove all cells absent of mean values
-    out2 <- out2[rowSums(is.na(out2)[,4:ncol(data)]) < (max(4:ncol(data))-3),]
-    return(out2)
-  }
-
-#if (nrow(spdf@coords) == 1) {
-#  # 
-#  rasN1 <- ras(spdf, mean, km, e) 
-#  
-#  d <- cbind(Species = as.character(data[,1]), cellID = NA, data[,2:ncol(data)])
-#  d[,4:3] <- df.spp@bbox[1:2]
-#  print("n1")
-#  
-#} else {
+rMGrid <- function(data, km, e) {
+  # Check for species with layers that are all NAs
+  checkNA(data)
+  
+  # Convert to spatial dataframe
+  spdf <- spdf(data)
+  
+  # Calculate raster means and sds
+  rasMean <- ras(spdf, mean, km, e)
+  rasSD   <- ras(spdf, sd,   km, e)
+  
+  # Export cell ID and coordinates
+  outM  <- data.frame(1:nrow(rasMean@data@values ), xyFromCell(rasMean, 1:nrow(rasMean@data@values )),  rasMean@data@values)
+  outSD <- data.frame(1:nrow(rasSD@data@values), xyFromCell(rasSD,   1:nrow(rasSD@data@values)), rasSD@data@values)
+  
+  # Rename columns
+  colnames(outM )[1]   <- colnames(outSD)[1] <- "cellID"
+  colnames(outM )[4:ncol(data)] <- paste("mean", colnames(data)[4:ncol(data)], sep = "_")
+  colnames(outSD)[4:ncol(data)] <- paste("SD"  , colnames(data)[4:ncol(data)], sep = "_")
+  
+  # Join mean and SD
+  out2 <- left_join(outM, outSD[, c(1, 4:ncol(data))], "cellID")
+  
+  # Remove all cells absent of mean values
+  out2 <- out2[rowSums(is.na(out2)[,4:ncol(data)]) < (max(4:ncol(data))-3),]
+  return(out2)
+  
+}
 
 setExt <- function(data) {
   ext <- extent((min(data[,3])-2), (max(data[,3])+2),
